2c6be1d99d0ec92df598a7f90e2c1bc9e503a435
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-04-30 13:19:16 -0600

    working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
+import { config } from "dotenv";
+config();
+
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
 import Deck from "./models/Deck";
 
 const PORT = 5001;
-const uri =
-  "mongodb+srv://flashcardsage:DNEz7iUBg3TFXDBX@cluster0.wldemyp.mongodb.net/?retryWrites=true&w=majority";
+const mongoURL = process.env.MONGO_URL!;
 const app = express();
 
 //express middleware to parse json
@@ -22,7 +24,7 @@ app.post("/decks", async (req: Request, res: Response) => {
   res.json(createdDeck);
 });
 
-mongoose.connect(uri).then(() => {
+mongoose.connect(mongoURL).then(() => {
   console.log(`Connected to MongoDB on port ${PORT}`);
   app.listen(PORT);
 });

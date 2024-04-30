c23e3ebacc5a08ea9a4edb3b50253df4e97ccb0e
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-04-30 12:30:57 -0600

    idk

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import Deck from "./models/deck";
+
+const app = express();
+
+const db = mongoose.connect(
+  "mongodb+srv://flashcardsage:<DNEz7iUBg3TFXDBX>@cluster0.wldemyp.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(5001);

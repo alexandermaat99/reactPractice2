bd3373db020bf64436fae2cd63adcdd58553173d
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-04-30 12:51:27 -0600

    not working

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,22 @@ import mongoose from "mongoose";
 
 import Deck from "./models/Deck";
 
+const PORT = 5001;
+const uri =
+  "mongodb+srv://flashcardsage:DNEz7iUBg3TFXDBX@cluster0.wldemyp.mongodb.net/?retryWrites=true&w=majority";
+
 const app = express();
 
-const db = mongoose.connect(
-  "mongodb+srv://flashcardsage:<DNEz7iUBg3TFXDBX>@cluster0.wldemyp.mongodb.net/?retryWrites=true&w=majority"
-);
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+// Create a new deck - restfull architecture
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: "Deck 1",
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-app.listen(5001);
+mongoose.connect(uri).then(() => {
+  console.log(`Connected to MongoDB on port ${PORT}`);
+  app.listen(PORT);
+});
